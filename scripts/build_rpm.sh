84d16121bdb594c8c75c4254938a4179ed09b275
Sergey Kulanov <skulanov@mirantis.com>
2015-03-18 23:45:04 +0200

    Remove unnessassery mount to docker

--- scripts/build_rpm.sh
+++ scripts/build_rpm.sh
@@ -14,9 +14,8 @@ mkdir -p ${RESULT_DIR}
 
 for pckgs in $(ls ${SOURCE_PATH}); do
 docker run -v ${SOURCE_PATH}/${pckgs}:/opt/sandbox/SOURCES \
-             -v ${SPEC_FILE_PATH}/${pckgs}.spec:/opt/sandbox/$(basename ${pckgs}).spec \
-             -v ${SPEC_FILE_PATH}/${pckgs}:/opt/sandbox/${pckgs} \
-             -v ${RESULT_DIR}:/opt/sandbox/RPMS \
-             -u ${UID} \
-             -t -i fuel/rpmbuild_env rpmbuild --nodeps -vv --define "_topdir /opt/sandbox" -ba /opt/sandbox/$(basename ${pckgs}).spec
+           -v ${SPEC_FILE_PATH}/${pckgs}.spec:/opt/sandbox/$(basename ${pckgs}).spec \
+           -v ${RESULT_DIR}:/opt/sandbox/RPMS \
+           -u ${UID} \
+           -t -i fuel/rpmbuild_env rpmbuild --nodeps -vv --define "_topdir /opt/sandbox" -ba /opt/sandbox/$(basename ${pckgs}).spec
 done
